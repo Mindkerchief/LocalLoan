967f72226628d06a4cddc303c560067fe42af044
Jhondale Llaguno <jhondalellaguno@gmail.com>
2023-11-22 16:27:52 +0800

    Set light mode as default theme

--- app/src/main/java/com/lspuspcc/localloan/MainActivity.java
+++ app/src/main/java/com/lspuspcc/localloan/MainActivity.java
@@ -2,6 +2,7 @@ package com.lspuspcc.localloan;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         checkPermissions();
 
         fragmentTransaction = getSupportFragmentManager().beginTransaction();
